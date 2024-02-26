340f208ea2d799737a99243813d612a2093febd5
Lucas Fernando Gonlçaves Lima <lcs.lima0708@gmail.com>
2024-02-26 10:54:26 -0300

    s

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
 import { searchMovies } from "./api.js";
+import { svgButtonStar, svgButtonFavorite } from "./svgButton.js";
 
 const searchMoviesInput = document.querySelector(".form-search");
-const addImage = document.querySelector(".imgCardMovie");
-const addTitle = document.querySelector(".titleCardMovie");
-const addDescription = document.querySelector(".descriptionCardMovie");
+const sectionCard = document.querySelector(".sectionCardsMovie");
 
 searchMoviesInput.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const search = event.target[0].value;
   const movies = await searchMovies(search);
-  const movieImg = movies.results[0].poster_path;
-  const titleMovie = movies.results[0].title;
-  const descriptionMovie = movies.results[0].overview;
+  const results = movies.results;
 
-  addImage.src = `https://image.tmdb.org/t/p/w500${movieImg}`;
-  addTitle.textContent = titleMovie;
-  addDescription.textContent = descriptionMovie;
+  for (const movie of results) {
+    const movieImg = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    const titleMovie = movie.title;
+    const descriptionMovie = movie.overview;
+    const notaMovie = movie.vote_average;
 
-  console.log(movies);
+    const card = document.createElement("section");
+    card.classList.add("sectionCardsMovie");
+    card.innerHTML = `
+      <div class="cardMovie">
+        <div class="imgDivCardMovie">
+          <img src="${movieImg}" alt="Poster do filme ${titleMovie}" class="imgCardMovie" />
+        </div>
+  
+        <div class="notesAndButtonMovie">
+          <p class="titleCardMovie">${titleMovie}</p>
+          <div class="icons">
+            <span class="notesCard">
+              ${svgButtonStar}
+              ${notaMovie}
+            </span>
+            <button class="favoriteButtonMovie">
+              ${svgButtonFavorite}
+              Favoritar
+            </button>
+          </div>
+        </div>
+  
+        <p class="descriptionCardMovie">
+          ${descriptionMovie}
+        </p>
+      </div>`;
+    sectionCard.appendChild(card);
+  }
 });
